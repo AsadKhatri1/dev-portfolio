613ae8db149c1e4a96e87371bdd04e90cb70664e
Asad khatri <92842688+AsadKhatri1@users.noreply.github.com>
2024-07-06 17:14:13 +0500

    portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,92 +14,102 @@ const Portfolio = () => {
 
       <div className="flex md:flex-row flex-col  items-center justify-center md:w-full w-auto md:px-20 my-5 ">
         {/* 1st card */}
-        <div className="md:max-w-sm w-auto  rounded overflow-hidden shadow-lg bg-[#292F36] m-5">
-          <img
-            className="w-full max-h-[225px]"
-            src={manage}
-            alt="Sunset in the mountains"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2 text-[#12F7D6] ">
-              Directory Management System
+        <a href="https://directory--sigma.vercel.app/" target="_blank">
+          <div className="md:max-w-sm w-auto  rounded overflow-hidden shadow-lg bg-[#292F36] m-5">
+            <img
+              className="w-full max-h-[225px]"
+              src={manage}
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2 text-[#12F7D6] ">
+                Directory Management System
+              </div>
+              <p className="text-white text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
             </div>
-            <p className="text-white text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-[#1faa95]  rounded-full px-3 py-1 text-sm font-semibold text-[#ffffff]  mr-2 mb-2">
+                #mernstack
+              </span>
+              <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                #reactjs
+              </span>
+              <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                #fullstack
+              </span>
+            </div>
           </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-[#1faa95]  rounded-full px-3 py-1 text-sm font-semibold text-[#ffffff]  mr-2 mb-2">
-              #mernstack
-            </span>
-            <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              #reactjs
-            </span>
-            <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              #fullstack
-            </span>
-          </div>
-        </div>
+        </a>
         {/* 1st card */}
-        <div className="max-w-sm  rounded overflow-hidden shadow-lg  bg-[#292F36] m-5">
-          <img
-            className="w-full  max-h-[225px]"
-            src={threads}
-            alt="Sunset in the mountains"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2 text-[#12F7D6] ">
-              Threads & Fabrics Management
+        <a href="https://stock-client-kohl.vercel.app/" target="_blank">
+          <div className="max-w-sm  rounded overflow-hidden shadow-lg  bg-[#292F36] m-5">
+            <img
+              className="w-full  max-h-[225px]"
+              src={threads}
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2 text-[#12F7D6] ">
+                Threads & Fabrics Management
+              </div>
+              <p className="text-white text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
             </div>
-            <p className="text-white text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-[#1faa95]  rounded-full px-3 py-1 text-sm font-semibold text-[#ffffff]  mr-2 mb-2">
+                #mernstack
+              </span>
+              <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                #reactjs
+              </span>
+              <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                #fullstack
+              </span>
+            </div>
           </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-[#1faa95]  rounded-full px-3 py-1 text-sm font-semibold text-[#ffffff]  mr-2 mb-2">
-              #mernstack
-            </span>
-            <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              #reactjs
-            </span>
-            <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              #fullstack
-            </span>
-          </div>
-        </div>
+        </a>
         {/* 1st card */}
-        <div className="max-w-sm rounded overflow-hidden shadow-lg  bg-[#292F36] m-5 ">
-          <img
-            className="w-full h-[225px]"
-            src={ecommerce}
-            alt="Sunset in the mountains"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2 text-[#12F7D6] ">
-              ECommerce Store
+        <a
+          href="https://customdigitizers.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="max-w-sm rounded overflow-hidden shadow-lg  bg-[#292F36] m-5 ">
+            <img
+              className="w-full h-[225px]"
+              src={ecommerce}
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2 text-[#12F7D6] ">
+                ECommerce Store
+              </div>
+              <p className="text-white text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
             </div>
-            <p className="text-white text-base">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil.
-            </p>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-[#1faa95]  rounded-full px-3 py-1 text-sm font-semibold text-[#ffffff]  mr-2 mb-2">
+                #mernstack
+              </span>
+              <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                #reactjs
+              </span>
+              <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
+                #fullstack
+              </span>
+            </div>
           </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-[#1faa95]  rounded-full px-3 py-1 text-sm font-semibold text-[#ffffff]  mr-2 mb-2">
-              #mernstack
-            </span>
-            <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              #reactjs
-            </span>
-            <span className="inline-block bg-[#1faa95] rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">
-              #fullstack
-            </span>
-          </div>
-        </div>
+        </a>
       </div>
     </div>
   );
